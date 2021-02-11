ebd9d66029e89a9d67c62fdf1a731b1f15c764ef
Henry Hamilton Holmes <hholmes726@gmail.com>
2021-02-11 12:08:06 -0500

    Edited the root index.css, Flex-Container component, About-Container component, and About section with basic formatting.

--- v1_portfolio/src/pages/About/index.js
+++ v1_portfolio/src/pages/About/index.js
@@ -1,10 +1,16 @@
 import React from "react";
+import AboutContainer from "../../components/About-Container/index";
 import "./style.css";
 
 const About = () => {
     return (
         <>
-            <h1>Hello, world!</h1>
+            <AboutContainer>
+                <h1>Hello, world!</h1>
+                <p>
+                    Hoj je deka ofon, nu unua vortludo ekskluzive ies. Fiksa ekkria ili do. Is kuzo mega tre. Sob jo ator frida disskribado, ofon sekse artefarita iom zo. Ha baf kial pasko praantaŭlasta, plie prezoinda ge pre. Ac ligvokalo diskriminacio sob, ig zeta nenia senobjekta nea. Vi tiama infra bis, jeno egalas enz ol. Ing post danke festo o, enz kies ultra artefarita zo. Via ie peto stif kemia, halo pleja akuzativo kio nf.
+                </p>
+            </AboutContainer>
         </>
     );
 };
